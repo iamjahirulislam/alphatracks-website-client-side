a0257a390e889d647c241670ad213f462d2a56d5
iamjahirulislam <jahirul4694@gmail.com>
2021-11-27 18:30:02 +0600

    Updated Payment System

--- src/pages/Dashboard/Payment/Payment.js
+++ src/pages/Dashboard/Payment/Payment.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Payment = () => {
     return (
         <div>
-            <h2>Payment system add soon...</h2>
+            <h2>Payment system add soon....</h2>
         </div>
     );
 };
